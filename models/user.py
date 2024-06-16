5b119c7a7c9e68262eec5add5a21af5dac4cf3a8
Arnavdahiya <128586883+Arnavdahiya@users.noreply.github.com>
2024-06-16 16:34:45 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import datetime
 from typing import Optional
 
 class Product(BaseModel):
-    product_id: int
     product_name: str
     product_owner: str
     created_at: datetime
     status: bool
+    @validator("product_name", "product_owner")
+    def name_must_be_longer_than_two_chars(cls, v):
+        if len(v) < 3:
+            raise ValueError("must be longer than 2 characters")
+        return v
 
 class UpdateProduct(BaseModel):
     product_name: Optional[str] = None
